d8a484176f3cd8a745a36571eb6b940d576e972d
bmartinezparedes <bmartinezparedes@danielcastelao.org>
2021-02-10 20:34:06 +0100

    Boletin15 completo + mejoras

--- src/com/programacion/Boletines/Boletin15/Boletin15.java
+++ src/com/programacion/Boletines/Boletin15/Boletin15.java
@@ -3,7 +3,7 @@ package com.programacion.Boletines.Boletin15;
 public class Boletin15 {
 
     public static void main(String[] args) {
-        MetodosB15 m = new MetodosB15();
+        Metodos m = new Metodos();
 
         //APARTADO 1
         /*
@@ -13,12 +13,108 @@ public class Boletin15 {
         m.mostrarArrayReves(numeros);
         m.mostrarArrayDouble(numeros);
         */
-        //APARTADO 2
+        //APARTADO 2 y 3
+        /*String []nombreAlumnos=new String [Integer.parseInt(JOptionPane.showInputDialog("Numero de alumnos"))];
+        int []notas={m.numerosAleatoriosInt(),m.numerosAleatoriosInt(),m.numerosAleatoriosInt(),m.numerosAleatoriosInt(),m.numerosAleatoriosInt()};
+        int opcion;
+        do{
+            opcion=Integer.parseInt(JOptionPane.showInputDialog("Menu:\n0 para salir\n1 Crear lista alumnos\n2 Visualizar Nº Suspensos y Aprovados"
+                    + "\n3 Mostrar nota media de la clase\n4 Mostrar la mayor nota\n5 Mostar nota de un alumno determinado\n6 Mostrar lista de alumnos aprovados"));
+            switch(opcion){
+                case 0:System.exit(0);
+                    break;
+                case 1:  m.crearMatriz(nombreAlumnos);
+                    break;
+                case 2:  m.mostrarNotas(notas);
+                    break;
+                case 3:  m.calcularMedia(notas);
+                    break;
+                case 4:  m.notaMayor(notas);
+                    break;
+                case 5:  m.verNotaAlumno(notas, nombreAlumnos);
+                    break;
+                case 6: m.verAlumnosAprovados(notas, nombreAlumnos);
+                    break;
+                case 7: m.ordenarNotasDeMenosAmas(notas, nombreAlumnos);
+                    break;
+                case 8: m.amosarNotasYnombres(notas, nombreAlumnos);
+                    break;
+            }
+        }while(opcion!=0);*/
 
-        int[] notas = {m.numerosAleatoriosInt(), m.numerosAleatoriosInt(), m.numerosAleatoriosInt(), m.numerosAleatoriosInt(), m.numerosAleatoriosInt()};
-        m.mostrarArrayInt(notas);
-        m.mostrarNotas(notas);
-        m.calcularMedia(notas);
-        m.notaMayor(notas);
+        //Apartado 4
+        int opcion2;
+        opcion2 = m.calcularRestoDNI();
+        switch (opcion2) {
+            case 0:
+                System.out.println("Su letra es: T");
+                break;
+            case 1:
+                System.out.println("Su letra es: R");
+                break;
+            case 2:
+                System.out.println("Su letra es: W");
+                break;
+            case 3:
+                System.out.println("Su letra es: A");
+                break;
+            case 4:
+                System.out.println("Su letra es: G");
+                break;
+            case 5:
+                System.out.println("Su letra es: M");
+                break;
+            case 6:
+                System.out.println("Su letra es: Y");
+                break;
+            case 7:
+                System.out.println("Su letra es: F");
+                break;
+            case 8:
+                System.out.println("Su letra es: P");
+                break;
+            case 9:
+                System.out.println("Su letra es: D");
+                break;
+            case 10:
+                System.out.println("Su letra es: X");
+                break;
+            case 11:
+                System.out.println("Su letra es: B");
+                break;
+            case 12:
+                System.out.println("Su letra es: N");
+                break;
+            case 13:
+                System.out.println("Su letra es: J");
+                break;
+            case 14:
+                System.out.println("Su letra es: Z");
+                break;
+            case 15:
+                System.out.println("Su letra es: S");
+                break;
+            case 16:
+                System.out.println("Su letra es: Q");
+                break;
+            case 17:
+                System.out.println("Su letra es: V");
+                break;
+            case 18:
+                System.out.println("Su letra es: H");
+                break;
+            case 19:
+                System.out.println("Su letra es: L");
+                break;
+            case 20:
+                System.out.println("Su letra es: C");
+                break;
+            case 21:
+                System.out.println("Su letra es: K");
+                break;
+            case 22:
+                System.out.println("Su letra es: E");
+                break;
+        }
     }
 }
--- src/com/programacion/Boletines/Boletin15/MetodosB15.java
+++ src/com/programacion/Boletines/Boletin15/MetodosB15.java
@@ -1,5 +1,7 @@
 package com.programacion.Boletines.Boletin15;
 
+import javax.swing.*;
+
 public class MetodosB15 {
     //PARA APARTADO 1
     public double numerosAleatoriosDouble() {
@@ -27,13 +29,6 @@ public class MetodosB15 {
     public int numerosAleatoriosInt() {
         int num = (int) Math.floor(Math.random() * 10);
         return num;
-    }
-
-    public void mostrarArrayInt(int[] lista) {
-
-        for (int i = 0; i < lista.length; i++) {
-            System.out.println("Alumno " + (i + 1) + "---->" + lista[i]);
-        }
     }
 
     public void mostrarNotas(int[] lista) {
@@ -68,5 +63,77 @@ public class MetodosB15 {
             }
         }
         System.out.println(notaMayor);
+    }
+
+    //Apartado 3
+    public String pedirNombres() {
+        String nombre = JOptionPane.showInputDialog("Introduce nombre alumno");
+        return nombre;
+    }
+
+    public void verNotaAlumno(int[] notas, String[] nombres) {
+        String nombreBusqueda = pedirNombres();
+        for (int i = 0; i < notas.length; i++) {
+            for (int j = 0; j < nombres.length; j++) {
+                if (i == j) {
+                    if (nombres[j].equals(nombreBusqueda))
+                        System.out.println("Nombre: " + nombres[j] + " Nota: " + notas[i]);
+                }
+            }
+        }
+    }
+
+    public String[] crearMatriz(String[] nombres) {
+
+        for (int i = 0; i < nombres.length; i++) {
+            nombres[i] = pedirNombres();
+        }
+        return nombres;
+    }
+
+    public void verAlumnosAprovados(int[] notas, String[] nombres) {
+        for (int i = 0; i < notas.length; i++) {
+            if (notas[i] >= 5) {
+                System.out.println("Nombre: " + nombres[i] + " Nota: " + notas[i]);
+            }
+        }
+
+    }
+
+    public void ordenarNotasDeMenosAmas(int[] notas, String[] nombres) {
+        int auxNotas;
+        String auxNombres;
+        for (int i = 0; i < notas.length - 1; i++) {
+            for (int j = i + 1; j < notas.length; j++) {
+                if (notas[i] > notas[j]) {
+                    auxNotas = notas[i];
+                    notas[i] = notas[j];
+                    notas[j] = auxNotas;
+                    auxNombres = nombres[i];
+                    nombres[i] = nombres[j];
+                    nombres[j] = auxNombres;
+                }
+            }
+        }
+    }
+
+    public void amosarNotasYnombres(int[] notas, String[] nombres) {
+        for (int i = 0; i < notas.length; i++) {
+            System.out.println("Nombre: " + nombres[i] + " Nota: " + notas[i]);
+        }
+    }
+    //Ejercicio 4
+
+    public int calcularRestoDNI() {
+        String dni;
+        int letraDNI = 0;
+        do {
+            dni = JOptionPane.showInputDialog("Introduce tu NIF");
+            if (dni.length() == 8) {
+                letraDNI = Integer.parseInt(dni) % 23;
+            } else
+                System.out.println("NIF incorrecto");
+        } while (dni.length() != 8);
+        return letraDNI;
     }
 }
